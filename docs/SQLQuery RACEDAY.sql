aed62b833f6549c9902781e669910872a8500596
st10474528 <st10474528@rcconnect.edu.za>
2026-09-04 18:39:40 +0200

    event table and event type

--- docs/SQLQuery RACEDAY.sql
+++ docs/SQLQuery RACEDAY.sql
@@ -31,3 +31,54 @@ CREATE TABLE [User]
         CHECK (Role IN ('Organiser', 'Participant'))
 );
 GO
+-- EVENT TYPE TABLE --
+ 
+
+CREATE TABLE EventType
+(
+    EventTypeID INT IDENTITY(1,1) PRIMARY KEY,
+
+    TypeName NVARCHAR(20) NOT NULL UNIQUE
+);
+GO
+
+
+
+   -- EVENT TABLE--
+   
+
+CREATE TABLE Event
+(
+    EventID INT IDENTITY(1,1) PRIMARY KEY,
+
+    OrganiserID INT NOT NULL,
+
+    EventTypeID INT NOT NULL,
+
+    Name NVARCHAR(150) NOT NULL,
+
+    Description NVARCHAR(1000) NULL,
+
+    EventDate DATE NOT NULL,
+
+    Location NVARCHAR(200) NOT NULL,
+
+    Distance DECIMAL(6,2) NOT NULL,
+
+    BannerImageUrl NVARCHAR(500) NULL,
+
+    CreatedAt DATETIME2 NOT NULL
+        CONSTRAINT DF_Event_CreatedAt DEFAULT SYSDATETIME(),
+
+    CONSTRAINT FK_Event_Organiser
+        FOREIGN KEY (OrganiserID)
+        REFERENCES [User](UserID),
+
+    CONSTRAINT FK_Event_EventType
+        FOREIGN KEY (EventTypeID)
+        REFERENCES EventType(EventTypeID),
+
+    CONSTRAINT CK_Event_Distance
+        CHECK (Distance > 0)
+);
+GO
